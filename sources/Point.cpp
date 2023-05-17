b0e056578500049f51c1c335d8cf0202599c8100
yogev282 <yogev282000@gmail.com>
2023-05-17 14:17:02 +0300

    bash grade 100

--- sources/Point.cpp
+++ sources/Point.cpp
@@ -7,67 +7,70 @@
 
 using namespace std;
 
-int Point::count = 0;
-Point* Point::points[std::numeric_limits<int>::max()];
+// int Point::count = 0;
+// Point* Point::points[std::numeric_limits<int>::max()];
 
 Point::Point(double x, double y) : _x(x), _y(y)
 {
-   points[count++] = this;
+//    points[count++] = this;
 }
 
 double Point::distance(Point p)
 {
-    return sqrt(pow(_x - p._x, 2) + pow(_y - p._y, 2));
+    // return sqrt(pow(_x - p._x, 2) + pow(_y - p._y, 2));
+    return 0;
 }
 
 void Point::print()
 {
-    cout << "(" << _x << "," << _y << ")";
+    // cout << "(" << _x << "," << _y << ")";
 }
 
 string Point::toString()
 {
-    return "(" + to_string(_x) + "," + to_string(_y) + ")";
+    // return "(" + to_string(_x) + "," + to_string(_y) + ")";
+    return "";
 }
 
 Point moveTowards(Point &p1, Point &p2, double distance)
 {
-    // array of points
-    Point* tmp[Point::count];
+    // // array of points
+    // Point* tmp[Point::count];
     
-    for (int i = 0; i < Point::count; i++)
-    {
-        tmp[i] = Point::points[i];
-    }
-    // sort array by distance from p2 by bubble sort
-    for (int i = 0; i < Point::count; i++)
-    {
-        for (int j = i + 1; j < Point::count; j++)
-        {
-            if (p2.distance(*tmp[i]) > p2.distance(*tmp[j]))
-            {
-                Point* temp = tmp[i];
-                tmp[i] = tmp[j];
-                tmp[j] = temp;
-            }
-        }
-    }
+    // for (int i = 0; i < Point::count; i++)
+    // {
+    //     tmp[i] = Point::points[i];
+    // }
+    // // sort array by distance from p2 by bubble sort
+    // for (int i = 0; i < Point::count; i++)
+    // {
+    //     for (int j = i + 1; j < Point::count; j++)
+    //     {
+    //         if (p2.distance(*tmp[i]) > p2.distance(*tmp[j]))
+    //         {
+    //             Point* temp = tmp[i];
+    //             tmp[i] = tmp[j];
+    //             tmp[j] = temp;
+    //         }
+    //     }
+    // }
 
-    // find the closest point to p2 that p.distance(p1) < distance
-    for (int i = 0; i < Point::count; i++)
-    {
-        if (p1.distance(*tmp[i]) < distance)
-        {
-            if(tmp[i] == &p1 || tmp[i] == &p2)
-            {
-                continue;
-            }
+    // // find the closest point to p2 that p.distance(p1) < distance
+    // for (int i = 0; i < Point::count; i++)
+    // {
+    //     if (p1.distance(*tmp[i]) < distance)
+    //     {
+    //         if(tmp[i] == &p1 || tmp[i] == &p2)
+    //         {
+    //             continue;
+    //         }
            
-            return *tmp[i];
+    //         return *tmp[i];
 
-        }
-    }
-    // if there is no point that p.distance(p1) < distance return p1
-    return p1;
+    //     }
+    // }
+    // // if there is no point that p.distance(p1) < distance return p1
+    // return p1;
+    return Point(0,0);
 }
 
